f85c5c7ad24b476ce206c10369e7d7ea6fa8a930
wald <1000druid@gmail.com>
2019-11-17 20:51:49 +0100

    adding widget_tweaks to forms

--- shop/forms.py
+++ shop/forms.py
@@ -5,8 +5,3 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
-
-    def __init__(self, *args, **kwargs):
-        super(CommentForm, self).__init__(*args, **kwargs)
-        self.fields['content'].widget.attrs = {'class':'form-control', 'rows': 10}
-        self.fields['content'].label = "Treść opinii"
